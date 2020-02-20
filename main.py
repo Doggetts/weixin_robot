a6a4fc73c8ee269d7da099a578b68ba28993a345
home <1343612280@qq.com>
2020-02-20 18:43:31 +0800

    帮助文档

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-import datetime
-print("ok")
+from urllib import request
+from urllib import parse
+from urllib.request import urlopen
+import urllib3,json,urllib
+url = "http://open.iciba.com/dsapi/"
+#result = urllib.request.urlopen(url)
+result = urllib.request.urlopen(url)
+jsonarr = json.loads(result.read())
+print (jsonarr)
+print("ok ")
